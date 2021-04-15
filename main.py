6dcac32b483bf13acaddaae32ee169540c33ba8d
Hlonela <hloni.mabhani@gmail.com>
2021-04-15 16:07:58 +0200

    latest edits

--- main.py
+++ main.py
@@ -10,5 +10,8 @@ import math
 
 print("Pythagoras is: \n", math.sqrt(answer)) #// ac = (ab**2 + bc**2) ** (1/2)
 
-#THAPELO'S
+#TASK TWO:
 
+list1 = [2,56,12,67, 1000, 32, 89, 29, 44, 39, 200, 11, 21]
+print(" Max number in my list is: /n" + str (max (list1)))
+print( "Min number in my list is: /n" + str (min (list1)))
