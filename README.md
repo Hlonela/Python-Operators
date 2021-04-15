f51dc3eb05e76a95195ee9b2a57129ebe490d95f
Hlonela <hloni.mabhani@gmail.com>
2021-04-15 11:58:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python-Operators
+# Python-Operators
---
+++ main.py
@@ -0,0 +1,19 @@
+#TASK ONE:
+
+ab = float (input(" Please insert sideAB: \n"))# The user inserts
+bc = float (input("Please insert sideBC: \n"))
+
+if (ab >= 0 and bc >= 0) :
+    answer = ab**2 + bc **2
+    print(answer)
+import math
+
+print("Pythagoras is: \n", math.sqrt(answer)) #// ac = (ab**2 + bc**2) ** (1/2)
+
+#THAPELO'S
+
+ Commit directly to the main branch.
+
+
+
+git
